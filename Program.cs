531c7dc45543a35cd6c1f857721b992b12476ad1
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-05 19:32:10 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationDotnetAssignment1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.SetCursorPosition((Console.WindowWidth) / 2, Console.CursorTop); //This line is being used to place the "Login Please" text 
+        Console.WriteLine("Login Please");
+    }
+}
---
+++ UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ApplicationDotnetAssignment1
+{
+    public class UserContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DAVIDTHINKPAD; Database=HospitalManagementSystem; User Id=David; Password=test; TrustServerCertificate=True");
+        }
+
+        public void CheckIfUserCredientalsAreCorrect(string userId, string password)
+        {
+        }
+    }
+}
