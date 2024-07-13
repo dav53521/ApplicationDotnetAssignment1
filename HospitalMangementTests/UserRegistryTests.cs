df42c9ab2347a0f295b22cc4b56180748059541c
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-13 21:52:40 +1000

    Automatic library updates and also start adding tests for registry

---
+++ HospitalMangementTests/UserRegistryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMangementTests
+{
+    public class UserRegistryTests
+    {
+        [Test]
+        public void UserRegistryCanGetAllUsers()
+        {
+        }
+    }
+}
--- HospitalMangementTests/obj/Debug/net8.0/HospitalMangementTests.AssemblyInfo.cs
+++ HospitalMangementTests/obj/Debug/net8.0/HospitalMangementTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e72841ca7350596bee6b06d6f103e30d820c8489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94beb7cae66a9c92fdd4fa39316d7d0683523f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
