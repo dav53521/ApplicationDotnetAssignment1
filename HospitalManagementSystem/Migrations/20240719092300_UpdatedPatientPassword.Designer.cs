62ec0252c006997bfc9bf6552faa1d84e16152c1
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-19 19:42:18 +1000

    Add migrations

--- HospitalManagementSystem/Migrations/20240719092300_UpdatedPatientPassword.Designer.cs
+++ HospitalManagementSystem/Migrations/20240719092300_UpdatedPatientPassword.Designer.cs
@@ -4,6 +4,7 @@ using ApplicationDotnetAssignment1.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApplicationDotnetAssignment1.Migrations
 {
     [DbContext(typeof(HospitalSystemContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20240719092300_UpdatedPatientPassword")]
+    partial class UpdatedPatientPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HospitalManagementSystem/Migrations/20240719092300_UpdatedPatientPassword.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationDotnetAssignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedPatientPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Password",
+                value: "test");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Password",
+                value: "123");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Password",
+                value: "Jane");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Password",
+                value: "1");
+        }
+    }
+}
