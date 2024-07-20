cf4ddb11c8e50ffb9f834a634809f969003e6a42
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-20 16:17:41 +1000

    Create email service

---
+++ HospitalManagementSystem/Services/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationDotnetAssignment1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDotnetAssignment1.Services
+{
+    public static class EmailService
+    {
+        public static void SendAppointmentEmail(Appointment bookedAppointment)
+        {
+        }
+    }
+}
