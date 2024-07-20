080894ae22a2f6602a30b0702627144e2f4f06cb
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-20 19:09:40 +1000

    Update doctor service

--- HospitalManagementSystem/Services/DoctorService.cs
+++ HospitalManagementSystem/Services/DoctorService.cs
@@ -19,7 +19,7 @@ namespace ApplicationDotnetAssignment1.Services
         {
             Console.WriteLine(@"1. List Doctor Details
 2. List patients
-3. List appointmnets
+3. List appointments
 4. Check particular patient
 5. List appointments with patient
 6. Logout
@@ -63,60 +63,26 @@ namespace ApplicationDotnetAssignment1.Services
         void PrintAssignedPatients()
         {
             Console.Clear();
-            Console.SetCursorPosition((Console.WindowWidth / 2) - 5, Console.CursorTop);
-            Console.WriteLine("Assigned Patient Details");
-            Console.WriteLine("{0,-30} | {1,-30} | {2,-30} | {3,-10} | {4}", "Name", "Doctor", "Email Address", "Phone", "Address");
-
-            for (int i = 0; i < Console.WindowWidth; i++)
-            {
-                Console.Write("-");
-            }
-
+            ConsoleService.PrintInCenter("Assigned Patients");
             LoggedInUser.Patients.PrintAllValidElements();
-
-            Console.WriteLine();
-            Console.WriteLine("Please press any key to return back to the main menu");
-            Console.ReadKey();
+            ConsoleService.WaitForKeyPress();
         }
 
         void PrintDoctorDetails()
         {
             Console.Clear();
-            Console.WriteLine("{0,-30} | {1,-30} | {2,-10} | {3}", "Name", "Email Address", "Phone", "Address");
-            for (int i = 0; i < Console.WindowWidth; i++)
-            {
-                Console.Write("-");
-            }
-
+            Console.WriteLine("{0,-30} | {1,-30} | {2,-50} | {3}", "Name", "Email Address", "Address", "Phone");
+            ConsoleService.PrintSeperator();
             Console.WriteLine(LoggedInUser.ToString());
-
-            Console.WriteLine();
-            Console.WriteLine("Please press any key to return back to the main menu");
-            Console.ReadKey();
+            ConsoleService.WaitForKeyPress();
         }
 
         void PrintAssignedAppointments()
         {
             Console.Clear();
-            Console.SetCursorPosition((Console.WindowWidth / 2) - 5, Console.CursorTop);
-            Console.WriteLine("All Appointments");
-            Console.WriteLine("{0,-30} | {1,-30} | {2}", "Doctor", "Patient", "Description");
-
-            for (int i = 0; i < Console.WindowWidth; i++)
-            {
-                Console.Write("-");
-            }
-
+            ConsoleService.PrintInCenter("All Appointments");
             LoggedInUser.AssignedAppointments.PrintAllElements();
-
-            Console.WriteLine();
-            Console.WriteLine("Please press any key to return back to the main menu");
-            Console.ReadKey();
-        }
-
-        void CheckParticularPatient()
-        {
-            ConsoleService.GetIntegerFromUser("Enter ID of patient to check", "Please only enter numbers");
+            ConsoleService.WaitForKeyPress();
         }
     }
 }
