910f671c0873d1e233f18b28f5a91a70148216d8
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-20 19:18:57 +1000

    Finish implementing email service

--- HospitalManagementSystem/Services/EmailService.cs
+++ HospitalManagementSystem/Services/EmailService.cs
@@ -14,13 +14,18 @@ namespace ApplicationDotnetAssignment1.Services
 {
     public static class EmailService
     {
-        public static void SendAppointmentConfirmationEmail(Appointment bookedAppointment, HospitalSystemUnitOfWork unitOfWork)
+        public static bool TrySendAppointmentConfirmationEmail(Appointment bookedAppointment, HospitalSystemUnitOfWork unitOfWork)
         {
             try
             {
-                (Patient bookedPatient, Doctor assignedDoctor) = GetPatientAndDoctorFromAppointment(unitOfWork, bookedAppointment.PatientId, bookedAppointment.DoctorId);
+                (Patient patient, Doctor doctor) = GetPatientAndDoctorFromAppointment(unitOfWork, bookedAppointment.PatientId, bookedAppointment.DoctorId);
 
-                string subject = $"Appointment confirmation for {bookedPatient.Name}";
+                string subject = $"Appointment confirmation for {patient.Name}";
+
+                string body = @$"Dear {patient.Name},
+You have sucessfully booked an appointment with {doctor.Name} for the reason {bookedAppointment.Description}
+Sincerely,
+Hospital Management System";
 
                 string fromEmail = "davidhospitalmanagmentsystem@gmail.com";
 
@@ -36,15 +41,20 @@ namespace ApplicationDotnetAssignment1.Services
                 {
                     From = new MailAddress(fromEmail),
                     Subject = subject,
+                    Body = body,
+                    IsBodyHtml = false,
                 };
 
-                mailMessage.To.Add(new MailAddress(bookedPatient.Email));
+                mailMessage.To.Add(new MailAddress(patient.Email));
                 client.Send(mailMessage);
+
+                return true;
             }
             //The when is being used so that it's possible to catch multiple exception types in the one catch statement and also ensure that exceptions that shouldn't be caught such as "out of memory exception" are not caught as they should stop the program
             catch (Exception ex) when (ex is SmtpException or InvalidOperationException or SmtpFailedRecipientException or SmtpFailedRecipientsException)
             {
                 Console.WriteLine($"The email failed to send because an error occured: {ex.Message}");
+                return false;
             }
         }
 
