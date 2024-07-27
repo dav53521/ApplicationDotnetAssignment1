465e6239f5b3c28f286e31d61afece91212becd9
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-07-27 17:58:07 +1000

    Added tests for each repository

--- HospitalMangementTests/obj/Debug/net8.0/HospitalMangementTests.AssemblyInfo.cs
+++ HospitalMangementTests/obj/Debug/net8.0/HospitalMangementTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6191e94bd5087440376fc64e9869e28df9d745c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537d3e6ce3096219ee56d8a301cc8f34c0073d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalMangementTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
